438c26282f01ce423d6479dd3f8c49cb571f27b5
crazypoo <273277355@qq.com>
2015-02-04 23:45:11 +0800

    加入强力的颜色设置!!!!!!加入pch文件!!!!!!

--- JI/ViewController.m
+++ JI/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor warmGrayColor];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
